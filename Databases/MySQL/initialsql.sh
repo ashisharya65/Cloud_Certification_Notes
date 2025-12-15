3a53e2cce3b26a7b484a0479be4945259ac17f27
Ashish Arya <56192979+ashisharya65@users.noreply.github.com>
2025-12-15 21:56:02 +0530

    Add initial MySQL setup script

---
+++ Databases/MySQL/initialsql.sh
@@ -0,0 +1,15 @@
+
+# update package index
+sudo apt update
+
+# Install mysql server
+sudo apt install mysql-server
+
+# secure the installation 
+sudo mysql_secure_installation
+
+# Start the MySQL client as root on the local machine using sudo privileges
+sudo mysql
+
+# Log in to MySQL as user 'aarya1' and prompt for the password
+mysql -u aarya1 -p
